91db2267a98eb653cec3424873204deda748f00e
Villy Siu <villysiu@gmail.com>
2017-04-15 14:46:38 -0700

    create welcome and user input menu

--- lib/CookingClasses/cli.rb
+++ lib/CookingClasses/cli.rb
@@ -1,5 +1,31 @@
 class CookingClasses::CLI
-  def say_SurLaTable
-    puts "SurLaTable"
+  def call
+    list_lessons
+    display_user_menu
+  end
+
+  def list_lessons
+    puts "***************** Sur La Table **********************"
+    puts "******* Customer Favorite Cooking Classes ***********"
+    puts ""
+  end
+
+  def display_user_menu
+    input = nil
+    while input != "exit"
+      puts "Please type [lesson number] you'd like more information, or [list] to see all the lessons, or [exti] to exit the program"
+      input = gets.strip.downcase
+      case input
+      when "list"
+        list_lessons
+      when "exit"
+        puts "good bye"
+      when input.to_i.between?(1, 12)
+        #display_details(input.to_i)
+        puts input
+      else
+        puts "This is not a valid input. Try again."
+      end
+    end
   end
 end
