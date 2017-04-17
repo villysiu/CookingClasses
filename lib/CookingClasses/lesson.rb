277b2cda8103f42c449ab1308aae86192fddef47
Villy Siu <villysiu@gmail.com>
2017-04-17 15:39:25 -0700

    add new function
    add to return array or scrape data to create a new array. 
    scrape_lessons to scrape data from the website

--- lib/CookingClasses/lesson.rb
+++ lib/CookingClasses/lesson.rb
@@ -1,6 +1,7 @@
 class CookingClasses::Lesson
   attr_accessor :name, :url, :description, :menu
   @@all = []
+  @@base_url = "https://www.surlatable.com/"
 
   def initialize (name = nil, url = nil, description = nil, menu = nil)
     @name = name
@@ -10,5 +11,22 @@ class CookingClasses::Lesson
     @@all << self
   end
 
-  
+  def self.all
+    @@all.empty? ? scrape_lessons : @@all
+  end
+
+  def self.scrape_lessons
+    doc = Nokogiri::HTML(open(@@base_url + "category/cat2211278/In+Store+Classes"))
+
+    lesson_array = doc.css("div.boxsides").first
+    #binding.pry
+    lesson_array.css("div.product a").each do |l|
+      lesson_obj = self.new
+      lesson_obj.name = l.children[1]['alt'].sub!('Cooking Class: ', '')
+      lesson_obj.url = l['href']
+    end
+    @@all
+  end
+
+
 end
