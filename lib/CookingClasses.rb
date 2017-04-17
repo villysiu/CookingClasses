99da2db906fe82b2c69be2dd060f2b065e58542e
Villy Siu <villysiu@gmail.com>
2017-04-17 15:40:00 -0700

    require gem

--- lib/CookingClasses.rb
+++ lib/CookingClasses.rb
@@ -1,3 +1,7 @@
-#require_relative "./CookingClasses/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./CookingClasses/version"
 require_relative "./CookingClasses/cli"
 require_relative "./CookingClasses/lesson"
